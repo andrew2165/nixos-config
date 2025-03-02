a75dede70d7815b480c8330248532abbd1fec786
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro.local>
2025-03-02 11:51:32 -0500

    attempted generators re-organized into flakes

--- flake.nix
+++ flake.nix
@@ -19,48 +19,6 @@
 
   outputs =
     { self, nixpkgs, home-manager, nixos-generators, ... } @ input: {
-
-        # TODO: all the nixos generators stuff can probably get reformatted into a flake
-        # see https://github.com/nix-community/nixos-generators?tab=readme-ov-file
-        # Formats used by Nixos-generators as a module
-        # A single nixos config outputting multiple formats.
-        # Alternatively put this in a configuration.nix.
-        nixosModules.myFormats = { config, ... }: {
-          imports = [
-            nixos-generators.nixosModules.all-formats
-          ];
-
-          nixpkgs.hostPlatform = "x86_64-linux";
-
-          formatConfigs.nixosISO = { config, modulesPath, ... }: {
-            imports = [ 
-              "${toString modulesPath}/installer/cd-dvd/installation-cd-base.nix"
-              ./liveISO/configuration.nix
-              {virtualisation.diskSize = 10 * 1024;} # this might work, not sure untested
-            ];
-            formatAttr = "isoImage";
-            fileExtension = ".iso";
-          };
-
-          # customize an existing format
-          # formatConfigs.vmware = { config, ... }: {
-          #   services.openssh.enable = true;
-          #   modules = [
-          #     ./liveISO/configuration.nix
-          #   ];
-          # };
-
-          # define a new format
-          #formatConfigs.my-custom-format = { config, modulesPath, ... }: {
-          #  imports = [ "${toString modulesPath}/installer/cd-dvd/installation-cd-base.nix" ];
-          # formatAttr = "isoImage";
-          #  fileExtension = ".iso";
-          #  networking.wireless.networks = {
-              # ...
-          #  };
-          #};
-        };
-      
       # Defining my Nixos Configurations
       nixosConfigurations = { 
         nixosVM = nixpkgs.lib.nixosSystem {
@@ -84,11 +42,15 @@
         # https://github.com/nix-community/nixos-generators
         # run ex: nix build .#nixosConfigurations.my-machine.config.formats.vmware
         # for this one: nix build .#nixosConfigurations.nixosISO.config.formats.nixosISO
-        nixosISO = nixpkgs.lib.nixosSystem {
+      };
+
+      packages.x86_64-linux = {
+        liveISO = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
           modules = [
-            self.nixosModules.myFormats
-            #./liveISO/configuration.nix
+            ./liveISO/configuration.nix
           ];
+          format = "iso";
         };
       };
     };
