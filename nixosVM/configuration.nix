ef3bc48838a9a4568f67eb4bf9df35dd145843dc
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro.local>
2025-05-19 07:18:42 -0400

    added flatpaks to nixosVM

--- nixosVM/configuration.nix
+++ nixosVM/configuration.nix
@@ -140,6 +140,8 @@
 
   services.tailscale.enable = true;
 
+  services.flatpak.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
