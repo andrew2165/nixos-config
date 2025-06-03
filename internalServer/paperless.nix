06b096b8f7c171738d565a17f4169dab4a14147e
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro.local>
2025-06-02 20:42:19 -0400

    tried new mounting scheme

--- internalServer/paperless.nix
+++ internalServer/paperless.nix
@@ -1,16 +1,32 @@
 { config, pkgs, ... }: {
 
     networking.firewall.extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
-    fileSystems."/mnt/paperless" = {
-        device = "//100.113.228.33/paperless";
-        fsType = "cifs";
+    # fileSystems."/mnt/paperless" = {
+    #     device = "//100.113.228.33/paperless";
+    #     fsType = "cifs";
+    #     options = let
+    #         # this line prevents hanging on network split
+    #         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users,file_mode=0770,dir_mode=0770";
+    #         # This "karakeep" pwd is for this server in general
+    #         credentials = config.age.secrets.tanker-karakeep-smb-pswd.path;
+    #     in ["${automount_opts},credentials=${credentials},uid=1000,gid=100"];
+    # };
+
+    # https://discourse.nixos.org/t/systemd-mounts-and-systemd-automounts-options-causing-an-error/13796/2
+    systemd.mounts = [{
+        description = "Paperless mount";
+        what = "//100.113.228.33/paperless";
+        where = "/mnt/paperless";
+        type = "cifs";
         options = let
-            # this line prevents hanging on network split
-            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users,file_mode=0770,dir_mode=0770";
-            # This "karakeep" pwd is for this server in general
-            credentials = config.age.secrets.tanker-karakeep-smb-pswd.path;
-        in ["${automount_opts},credentials=${credentials},uid=1000,gid=100"];
-    };
+                credentials = config.age.secrets.tanker-karakeep-smb-pswd.path;
+            in ["credentials = ${credentials}, rw"];
+    }];
+    systemd.automounts = [{
+        description = "Automount for Paperless NGX";
+        where = "/mnt/paperless";
+        wantedBy = [ "multi-user.target" ];
+    }];
 
     age.secrets.paperless.file = ../secrets/paperless.age;
 
