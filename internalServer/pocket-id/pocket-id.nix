b6cceeff0576dde16f75a63791c63963f7aabf5c
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro.local>
2025-06-07 22:06:10 -0400

    changed pocket-id to be a docker container

--- internalServer/pocket-id/pocket-id.nix
+++ internalServer/pocket-id/pocket-id.nix
@@ -1,17 +1,23 @@
 { config, pkgs, ... }: {
 
-
-    # TODO:finish configuration
-
     age.secrets.pocket-id-env = {
         file = ./../../secrets/pocket-id-env.age;
+        path = "/etc/pocket-id/.env";
     };
 
-    services.pocket-id = {
-        enable = true;
-        user = "andrew";
-        environmentFile = config.age.secrets.pocket-id-env.path;
-        dataDir = "/home/andrew/pocket-id";
+    systemd.services.pocket-id-docker-compose = {
+        path = [ 
+            pkgs.docker-compose
+            pkgs.docker
+        ];
+        script = ''
+        docker-compose -f /home/andrew/nixos-config/internalServer/pocket-id/docker-compose.yml up --detach
+        '';
+        wantedBy = ["multi-user.target"];
+        # If you use podman
+        #after = ["podman.service" "podman.socket"];
+        # If you use docker
+        after = ["docker.service" "docker.socket"];
     };
 
 }
