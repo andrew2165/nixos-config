28a0406b63fa4461738d62f75b6450ead1b6491c
Jonathan Stewart <jonathanstewart@jonathans-macbook-pro-1.tailc2733d.ts.net>
2025-06-15 21:14:00 -0400

    started implementing a backup strategy for mealie

--- internalServer/mealie/mealie.nix
+++ internalServer/mealie/mealie.nix
@@ -1,5 +1,30 @@
 { config, pkgs, ... }: {
 
+    # TODO: implement some kind of backup strategy for Mealie
+    # outside of the daily vm images
+    # https://github.com/mealie-recipes/mealie/discussions/4223
+    
+    # Just using the karakeep user 
+    age.secrets.tanker-mealie-smb-pswd = {
+        file = ./../../secrets/tanker-karakeep-smb-pswd.age;
+    };
+
+    systemd.mounts = [{
+        description = "mealie backup mount";
+        what = "//100.113.228.33/self-hosted-services/mealie";
+        where = "/mnt/mealie";
+        type = "cifs";
+        options = let
+                credentials = config.age.secrets.tanker-mealie-smb-pswd.path;
+            in "credentials=${credentials},rw,file_mode=0777,dir_mode=0777";
+    }];
+    systemd.automounts = [{
+        description = "Automount for Mealie";
+        where = "/mnt/mealie";
+        wantedBy = [ "multi-user.target" ];
+    }];
+
+
     age.secrets.mealie-env = {
         file = ./../../secrets/mealie-env.age;
         path = "/etc/mealie/mealie/.env";
@@ -8,9 +33,6 @@
         file = ./../../secrets/mealie-postgres-env.age;
         path = "/etc/mealie/postgres/.env";
     };
-
-    # TODO: implement some kind of backup strategy for Mealie
-    # outside of the daily vm images
 
 
     # Must login through the tailscale IP using the user/pass in iCloud keychain
