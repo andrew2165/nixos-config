3f47a08fe9145b65d9a45b6fe1f61df8bab9385a
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro.local>
2025-07-01 20:29:55 -0400

    added new things to vps config

--- wright-flyer2/configuration.nix
+++ wright-flyer2/configuration.nix
@@ -5,9 +5,13 @@
      experimental-features = "nix-command flakes";
    };
    
-   environment.systemPackages = [
-     pkgs.vim
-     pkgs.git
+   environment.systemPackages = with pkgs; [
+     vim
+     git
+     htop
+     fastfetch
+     podman-tui
+     podman-compose # start group of containers for dev
    ];
    
    fileSystems."/" = {
@@ -59,8 +63,24 @@
        KbdInteractiveAuthentication = false;
      };
    };
+
+   services.tailscale.enable = true;
    
    networking.firewall.allowedTCPPorts = [  ];
+
+    # Enable common container config files in /etc/containers
+    virtualisation.containers.enable = true;
+    virtualisation = {
+        podman = {
+            enable = true;
+
+            # Create a `docker` alias for podman, to use it as a drop-in replacement
+            dockerCompat = true;
+
+            # Required for containers under podman-compose to be able to talk to each other.
+            defaultNetwork.settings.dns_enabled = true;
+        };
+    };
    
    system.stateVersion = "25.05";
  }
