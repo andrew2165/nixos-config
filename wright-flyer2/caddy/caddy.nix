e8686ae1c076b93378e8f9aac40999000272ce57
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro.local>
2025-08-10 16:22:54 -0400

    trying as a systemd service

--- wright-flyer2/caddy/caddy.nix
+++ wright-flyer2/caddy/caddy.nix
@@ -1,34 +1,46 @@
 { config, pkgs, ... }: {
 
-    # Configure firewall port forwarding to allow for rootless container binding to lower ports
-    # https://wiki.nixos.org/wiki/Docker
-    boot.kernel.sysctl = {
-        "net.ipv4.conf.eth0.forwarding" = 1;    # enable port forwarding
+    services.caddy = {
+        enable = true;
+        package = pkgs.caddy.withPlugins {
+            plugins = [ 
+                "github.com/greenpau/caddy-security@1.1.31"
+                "github.com/hslatman/caddy-crowdsec-bouncer/crowdsec"
+            ];
+            hash = pkgs.lib.fakeSha256; # useful for finding the real one
+            # hash = "";
+        };
     };
+
+    # # Configure firewall port forwarding to allow for rootless container binding to lower ports
+    # # https://wiki.nixos.org/wiki/Docker
+    # boot.kernel.sysctl = {
+    #     "net.ipv4.conf.eth0.forwarding" = 1;    # enable port forwarding
+    # };
     
-    networking = {
-        firewall.extraCommands = ''
-            iptables -A PREROUTING -t nat -i eth0 -p TCP --dport 80 -j REDIRECT --to-port 8000
-            iptables -A PREROUTING -t nat -i eth0 -p TCP --dport 53 -j REDIRECT --to-port 5300
-            iptables -A PREROUTING -t nat -i eth0 -p UDP --dport 53 -j REDIRECT --to-port 5300
-        '';
-    };
+    # networking = {
+    #     firewall.extraCommands = ''
+    #         iptables -A PREROUTING -t nat -i eth0 -p TCP --dport 80 -j REDIRECT --to-port 8000
+    #         iptables -A PREROUTING -t nat -i eth0 -p TCP --dport 53 -j REDIRECT --to-port 5300
+    #         iptables -A PREROUTING -t nat -i eth0 -p UDP --dport 53 -j REDIRECT --to-port 5300
+    #     '';
+    # };
 
-    # Configure systemd service to build and start the container
-    systemd.services.caddy-container = {
-        path = [
-            pkgs.podman
-        ];
-        script = ''
-        podman compose -f /home/andrew/nixos-config/wright-flyer2/caddy/compose.yaml up --detach
-        '';
-        wantedBy = ["multi-user.target"];
-        # If you use podman
-        after = ["podman.service" "podman.socket"];
-        # If you use docker
-        # after = ["docker.service" "docker.socket"];
-    };
+    # # Configure systemd service to build and start the container
+    # systemd.services.caddy-container = {
+    #     path = [
+    #         pkgs.podman
+    #     ];
+    #     script = ''
+    #     podman compose -f /home/andrew/nixos-config/wright-flyer2/caddy/compose.yaml up --detach
+    #     '';
+    #     wantedBy = ["multi-user.target"];
+    #     # If you use podman
+    #     after = ["podman.service" "podman.socket"];
+    #     # If you use docker
+    #     # after = ["docker.service" "docker.socket"];
+    # };
 
-    # TODO: deploy caddy file using agenix
+    # # TODO: deploy caddy file using agenix
 
 }
