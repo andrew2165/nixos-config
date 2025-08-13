62ee1233acb95bbd993bbcfd834d72c9c6bdb06b
Jonathan Stewart <jonathanstewart@jonathans-macbook-pro.tailc2733d.ts.net>
2025-08-12 21:49:48 -0400

    attempting new user / perms structure for caddy

--- wright-flyer2/caddy/caddy.nix
+++ wright-flyer2/caddy/caddy.nix
@@ -1,16 +1,30 @@
 { config, pkgs, ... }: {
 
-    age.secrets.wright-flyer-caddyfile = {
-        file = ./../../secrets/wright-flyer-caddyfile.age
+    users.users = {
+        caddyier = {
+            name = "caddyier";
+            hashedPassword = "!";
+            uid = 678;
+            createHome = false;
+            isSystemUser = true;
+        };
     };
+
+    age.secrets.wright-flyer-caddyfile = {
+        file = ./../../secrets/wright-flyer-caddyfile.age;
+        path = "/etc/caddy/Caddyfile";
+    };
+
+    environment.etc."caddy/Caddyfile".uid = 678;
 
     # Issue with caddy plugins in sub directories & the corresponding go structure
     # that needed the install check to be overwridden.
     # Might be resolved in next release / unstable
     # https://github.com/NixOS/nixpkgs/issues/430090
     services.caddy = {
+        user = "caddyier";
         enable = true;
-        configFile = config.age.secrets.wright-flyer-caddyfile.path;
+        configFile = /etc/caddy/Caddyfile;
         package = (pkgs.caddy.withPlugins {
             plugins = [ 
                 "github.com/greenpau/caddy-security@v1.1.31"
