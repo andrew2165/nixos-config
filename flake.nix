dc71b241bf8fc2977918761bce8cd13467df9558
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro.local>
2025-10-01 17:09:32 -0700

    cant type a url

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
 
     nixpkgs-24-11.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     # home-manager, used for managing user configuration
