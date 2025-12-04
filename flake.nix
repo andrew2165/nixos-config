54dc26087500576bfa6456f91a52cc114a1e8354
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro-3.local>
2025-12-03 21:25:34 -0800

    updated base flake to 25.11

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
     nixpkgs-24-11.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     # home-manager, used for managing user configuration
     home-manager = {
