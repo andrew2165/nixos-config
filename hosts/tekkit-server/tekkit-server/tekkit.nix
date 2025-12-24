1a104067d7c3c90ce06b8706513943f2900ec570
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro-3.local>
2025-12-23 21:57:24 -0500

    added more documentation

--- hosts/tekkit-server/tekkit-server/tekkit.nix
+++ hosts/tekkit-server/tekkit-server/tekkit.nix
@@ -1,5 +1,11 @@
 { config, pkgs, ... }: {
 
+    # Quick note:
+    # This service does actually fail, I am not 100% sure why at the moment
+    # but I suspect it is because podman asks "hey which repo should I grab the base image from"
+    # and that waits and waits and so systemd kills the process... Doesn't matter bc I manually
+    # built the image that runs on the machine but good to know for the future.
+
     systemd.services.tekkit-podman-compose = {
         path = [ 
             pkgs.podman-compose
@@ -7,7 +13,7 @@
             pkgs.git
         ];
         script = ''
-        podman compose -f ${./docker-compose.yaml} up --build --detach
+        podman compose -f ${./docker-compose.yaml} up --detach
         '';
         wantedBy = ["multi-user.target"];
         # If you use podman
