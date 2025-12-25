4e9036aa9fdb20dadcb8a0019290dc9c1be54b17
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro-3.local>
2025-12-24 21:20:32 -0500

    trying compose2nix for tekkit podman setup

--- hosts/tekkit-server/configuration.nix
+++ hosts/tekkit-server/configuration.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }: {
 
     imports = [
-        ./tekkit-server/tekkit.nix
+        #./tekkit-server/tekkit.nix
+        ./tekkit-server/docker-compose.nix
     ];
 
     nix.settings = {
@@ -104,20 +105,20 @@
         ];
    };
 
+    # This section go moved to ./tekkit-server/docker-compose.nix
+    # # Enable common container config files in /etc/containers
+    # virtualisation.containers.enable = true;
+    # virtualisation = {
+    #     podman = {
+    #         enable = true;
 
-    # Enable common container config files in /etc/containers
-    virtualisation.containers.enable = true;
-    virtualisation = {
-        podman = {
-            enable = true;
+    #         # Create a `docker` alias for podman, to use it as a drop-in replacement
+    #         dockerCompat = true;
 
-            # Create a `docker` alias for podman, to use it as a drop-in replacement
-            dockerCompat = true;
-
-            # Required for containers under podman-compose to be able to talk to each other.
-            defaultNetwork.settings.dns_enabled = true;
-        };
-    };
+    #         # Required for containers under podman-compose to be able to talk to each other.
+    #         defaultNetwork.settings.dns_enabled = true;
+    #     };
+    # };
 
     networking.hostName = "tekkit-server";
     system.stateVersion = "25.11";
